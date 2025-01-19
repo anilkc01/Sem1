39924adf79351afdd0bd7cf0c6489f03466069a2
anilkc01 <mynameisanilkc@icloud.com>
2025-01-19 20:27:10 +0545

    signup page added

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
